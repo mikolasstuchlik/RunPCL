f7504b133cdaed03e8c4e2d23efc6655d66b163f
Mikoláš Stuchlík <stuchlej@gmail.com>
2022-08-05 19:26:08 +0200

    Modify for M1

--- Package.swift
+++ Package.swift
@@ -4,11 +4,25 @@
 import PackageDescription
 
 let pclRoot = "/Users/mikolasstuchlik/Developer/pcl"
-let pclBuildRoot = "/Users/mikolasstuchlik/Developer/pcl/build"
-let libInstallPath = "/usr/local/Cellar"
+let pclBuildRoot = "/Users/mikolasstuchlik/Developer/pcl/build_dy"
+
+let m1libIncludePath = "/opt/homebrew/Cellar"
+let x86libIncludePath = "/usr/local/Cellar"
+
+let m1libBinaryPath = "/opt/homebrew/Cellar"
+let x86libBinaryPath = "/usr/local/Cellar"
+
+
+#if arch(arm64)
+let includePath = m1libIncludePath
+let binaryPath = m1libBinaryPath
+#else
+let includePath = x86libIncludePath
+let binaryPath = x86libBinaryPath
+#endif
 
 // https://dev.my-gate.net/2021/08/04/understanding-rpath-with-cmake/
-// swift run -Xlinker -rpath -Xlinker /Users/mikolasstuchlik/Developer/pcl/build/lib
+// swift run -Xlinker -rpath -Xlinker {pclBuildRoot}/lib
 
 let package = Package(
     name: "RunPCL",
@@ -22,9 +36,9 @@ let package = Package(
                 // Each symbol (function declaration) you want to use nees to be .. declared.
                 // You can write those declarations yourself, or include additional header files in your project.
                 // Header files will be searched additionaly in folowing paths:
-                "-I\(libInstallPath)/boost/1.78.0_1/include",
-                "-I\(libInstallPath)/eigen/3.4.0_1/include/eigen3",
-                "-I\(libInstallPath)/flann/1.9.1_13/include",
+                "-I\(includePath)/boost/1.79.0_1/include",
+                "-I\(includePath)/eigen/3.4.0_1/include/eigen3",
+                "-I\(includePath)/flann/1.9.1_13/include",
                 "-I\(pclRoot)/common/include",
 				"-I\(pclRoot)/io/include",
 				"-I\(pclRoot)/build/include",
@@ -40,8 +54,8 @@ let package = Package(
                 // -L <dir> Add directory to library search path
                 // Comment:
                 // You can either provide full paths to libraries, or provide paths, where libraries are stored.
-                "-L\(libInstallPath)/boost/1.78.0_1/lib",
-                "-L\(libInstallPath)/flann/1.9.1_13/lib",
+                "-L\(binaryPath)/boost/1.79.0_1/lib",
+                "-L\(binaryPath)/flann/1.9.1_13/lib",
                 "-L\(pclBuildRoot)/lib",
                 // Comment:
                 // -l<library>
@@ -62,7 +76,10 @@ let package = Package(
         ),
         .executableTarget(
             name: "RunPCL", 
-            dependencies: ["CppPCL"]
+            dependencies: ["CppPCL"],
+            resources: [
+                .process("Resources")
+            ]
         ),
     ],
     cxxLanguageStandard: .cxx17
@@ -86,3 +103,27 @@ let package = Package(
 // -D CMAKE_CXX_FLAGS_RELEASE:STRING="-Ofast -DNDEBUG -funroll-loops -fno-strict-aliasing -ftree-vectorize -fomit-frame-pointer -march=native" \
 // -D CMAKE_C_FLAGS_RELEASE:STRING="-Ofast -DNDEBUG  -funroll-loops -fno-strict-aliasing -ftree-vectorize -fomit-frame-pointer -march=native" \
 // ..
+
+// Arm
+// Make static
+// cmake \
+// -D BUILD_surface_on_nurbs:BOOL=ON \
+// -D CMAKE_BUILD_TYPE:STRING=Release \
+// -D CMAKE_CXX_FLAGS_RELEASE:STRING="-Ofast -DNDEBUG -funroll-loops -fno-strict-aliasing -ftree-vectorize -fomit-frame-pointer" \
+// -D CMAKE_C_FLAGS_RELEASE:STRING="-Ofast -DNDEBUG  -funroll-loops -fno-strict-aliasing -ftree-vectorize -fomit-frame-pointer" \
+// -D PCL_SHARED_LIBS=OFF \
+// -D PCL_FLANN_REQUIRED_TYPE:STRING=STATIC \
+// -D PCL_QHULL_REQUIRED_TYPE:STRING=STATIC \
+// ..
+
+// Make dynamic
+// cmake \
+// -D BUILD_surface_on_nurbs:BOOL=ON \
+// -D CMAKE_BUILD_TYPE:STRING=Release \
+// -D CMAKE_CXX_FLAGS_RELEASE:STRING="-Ofast -DNDEBUG -funroll-loops -fno-strict-aliasing -ftree-vectorize -fomit-frame-pointer" \
+// -D CMAKE_C_FLAGS_RELEASE:STRING="-Ofast -DNDEBUG  -funroll-loops -fno-strict-aliasing -ftree-vectorize -fomit-frame-pointer" \
+// ..
+
+// Will generate dot files upon cmake generation.
+// Use graphviz to generate jpg: $ dot -Tjpg dep.graph > out.jpg
+// --graphviz=dep.graph
--- Sources/RunPCL/main.swift
+++ Sources/RunPCL/main.swift
@@ -1,7 +1,9 @@
 import CppPCL
+import Foundation
 
 //print("Hello, world!")
 //greetMe()
 //tryBoost()
 //runPcl("/Users/mikolasstuchlik/Downloads/RunPCL/RunPCL/Scan3.ply")
-publicFittingSurface("/Users/mikolasstuchlik/Downloads/RunPCL/RunPCL/Scan3.ply")
+let path = Bundle.module.url(forResource: "Scan3", withExtension: "ply")!
+publicFittingSurface(path.path)
